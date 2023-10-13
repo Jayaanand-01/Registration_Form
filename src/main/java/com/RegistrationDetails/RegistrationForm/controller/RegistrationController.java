db4a9813ce4ab38d89264bb4a72b29c554df4c45
Jaya Anand Natesan <anand.natesan@zucisystems.com>
2023-10-13 15:03:58 +0530

    Registration form

---
+++ src/main/java/com/RegistrationDetails/RegistrationForm/controller/RegistrationController.java
@@ -0,0 +1,14 @@
+package com.RegistrationDetails.RegistrationForm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/login")
+    public String home()
+    {
+        return "index";
+    }
+}
